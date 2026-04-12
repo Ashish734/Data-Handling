4751600c2df1aadf485ec7a61e7a84d86a764eea
Ashish734 <ashishkerketta069@gmail.com>
2026-04-12 23:52:28 +0530

    Delete P1.py

--- P1.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-url = "https://raw.githubusercontent.com/SR1608/Datasets/main/covid-data.csv"
-df=pd.read_csv(url)
-print(df.shape)
-print(df.dtypes)
-print(df.info())
-print(df.describe())
-
